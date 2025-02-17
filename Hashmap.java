0729a0abb216b942e4e0bcd83bd91ee8341ef6fd
Yamini Chowdary <hemanthkumar.raguru@gmail.com>
2025-02-17 14:25:39 +0530

    hashmap example

---
+++ Hashmap.java
@@ -0,0 +1,21 @@
+/*package practice;
+
+import java.util.HashMap;
+
+public class Hashmap {
+		public static void main(String[] args) {
+			HashMap<Integer, String> name = new HashMap<Integer, String>();
+			HashMap<String, String> names = new HashMap<String, String>();
+			name.put(24, "Hemanth");
+			name.put(32, "ram");
+			name.put(40, "abhi");
+			
+			names.put("N for", "Nandamuri");
+			names.put("T for", "Tarakaram");
+			names.put("R for","Rao");
+			
+			System.out.println("Mappings of HashMap name are:" +name);
+			System.out.println("Mappings of HashMap names are:" +names);
+		}
+		
+	}*/
